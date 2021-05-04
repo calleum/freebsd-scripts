02a2380412f04c0180296ba9b514b22721db6cf8
Calleum Pecqueux <calleum.p@protonmail.com>
2021-05-04 22:02:45 +1000

    add brightness script

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 ## ./status.sh _A FreeBSD Statusbar customised for thinkpad t480_
 
 Very simple statusbar script which displays the used memory, battery %, state and time remaining (this script is t480 specific in that it takes the battery values from both the internal and removable battery).
+
+## ./acpi_brightness_control.sh
+
+Use with your window manager, set the brightness keys (in my case 0x10, 0x11) in /etc/sysctl.conf:
+```
+    dev.acpi_ibm.0.handlerevents=0x10\ 0x11
+```
+To use these brightness keys, create /etc/devd/acpi_brightness.conf:
+```
+# /etc/devd/acpi_brightness.conf
+notify 20 {
+ match "system" "ACPI";
+ match "subsystem" "IBM";
+ match "notify" "0x10";
+ action "/usr/local/bin/acpi_brightness_control.sh 1";
+};
+notify 20 {
+ match "system" "ACPI";
+ match "subsystem" "IBM";
+ match "notify" "0x11";
+ action "/usr/local/bin/acpi_brightness_control.sh 0";
+}
+```
--- status.sh
+++ status.sh
@@ -1,11 +1,11 @@
-#!/usr/local/bin/bash
+#!/bin/sh
 
 # Display system information in statusbar of DWM
 # This script is combining shell re-write of
 ##  freebsd-memory -- List Total System Memory Usage
 ##  Copyright (c) 2003-2004 Ralf S. Engelschall <rse@engelschall.com>
 ## http://www.cyberciti.biz/files/scripts/freebsd-memory.pl.txt
-##  Copyright (c) 2015 Olivier Cochard-Labbé <olivier@freebsd.org>
+##  Copyright (c) 2015 Olivier Cochard-Labb�� <olivier@freebsd.org>
 ## (https://github.com/ocochard/myscripts/blob/master/FreeBSD/freebsd-memory.sh)
 # And https://github.com/anselal/homeunix/blob/master/.dwm/autostart.sh
 # To accurately follow the states available from acpiconf
@@ -16,14 +16,14 @@
 mem_rounded () {
     mem_size=$1
     chip_size=1
-    chip_guess=`echo "$mem_size / 8 - 1" | bc`
-    while [ $chip_guess != 0 ]
+    chip_guess=$(echo "$mem_size / 8 - 1" | bc)
+    while [ "$chip_guess" != 0 ]
         do
-                chip_guess=`echo "$chip_guess / 2" | bc`
-                chip_size=`echo "$chip_size * 2" | bc`
+                chip_guess=$(echo "$chip_guess / 2" | bc)
+                chip_size=$(echo "$chip_size * 2" | bc)
     done
-    mem_round=`echo "( $mem_size / $chip_size + 1 ) * $chip_size" | bc`
-    echo $mem_round
+    mem_round=$(echo "( $mem_size / $chip_size + 1 ) * $chip_size" | bc)
+    echo "$mem_round"
         exit 0
 }
 
@@ -32,75 +32,68 @@ free_memory () {
         #   NOTICE: forget hw.usermem, it is just (hw.physmem - vm.stats.vm.v_wire_count).
         #   NOTICE: forget vm.stats.misc.zero_page_count, it is just the subset of
         #           vm.stats.vm.v_free_count which is already pre-zeroed.
-        mem_phys=`sysctl -n hw.physmem`
+        mem_phys=$(sysctl -n hw.physmem)
         set +e
-        mem_hw=`mem_rounded $mem_phys`
+        mem_hw=$(mem_rounded "$mem_phys")
         set -e
-        sysctl_pagesize=`sysctl -n hw.pagesize`
-        mem_all=`echo "\`sysctl -n vm.stats.vm.v_page_count\` \
-	* $sysctl_pagesize" | bc`
-        mem_wire=`echo "\`sysctl -n vm.stats.vm.v_wire_count\` \
-	* $sysctl_pagesize" | bc`
-        mem_active=`echo "\`sysctl -n vm.stats.vm.v_active_count\` \
-	* $sysctl_pagesize" | bc`
-        mem_inactive=`echo "\`sysctl -n vm.stats.vm.v_inactive_count\` \
-	* $sysctl_pagesize" | bc`
-        mem_cache=`echo "\`sysctl -n vm.stats.vm.v_cache_count\` \
-	* $sysctl_pagesize" | bc`
-        mem_free=`echo "\`sysctl -n vm.stats.vm.v_free_count\` \
-	* $sysctl_pagesize" | bc`
+        sysctl_pagesize=$(sysctl -n hw.pagesize)
+        mem_inactive=$(echo "$(sysctl -n vm.stats.vm.v_inactive_count) \
+	* $sysctl_pagesize" | bc)
+        mem_cache=$(echo "$(sysctl -n vm.stats.vm.v_cache_count) \
+	* $sysctl_pagesize" | bc)
+        mem_free=$(echo "$(sysctl -n vm.stats.vm.v_free_count) \
+	* $sysctl_pagesize" | bc)
 
         #   determine logical summary information
         mem_total=$mem_hw
-        mem_avail=`echo "$mem_inactive + $mem_cache + $mem_free" | bc`
-        mem_used=`echo "$mem_total - $mem_avail" | bc`
+        mem_avail=$(echo "$mem_inactive + $mem_cache + $mem_free" | bc)
+        mem_used=$(echo "$mem_total - $mem_avail" | bc)
 
         #   print system results
-        mem_mb=`echo "$mem_used / ( 1024 * 1024 )" | bc`
-        if [ $mem_mb -lt 1000 ]; then
+        mem_mb=$(echo "$mem_used / ( 1024 * 1024 )" | bc)
+        if [ "$mem_mb" -lt 1000 ]; then
             printf "R: %7dMB [%3d%%]" \
-	`echo "$mem_used / ( 1024 * 1024 )" | bc` `echo "$mem_used \
-	* 100 / $mem_total" | bc`
+	"$(echo "$mem_used / ( 1024 * 1024 )" | bc)" "$(echo "$mem_used \
+	* 100 / $mem_total" | bc)"
         else
             printf "R: %.2fGB [%3d%%]" \
-	`echo "$mem_used / ( 1024 * 1024 * 1024 )" | bc -l` `echo "$mem_used \
-	* 100 / $mem_total" | bc`
+	"$(echo "$mem_used / ( 1024 * 1024 * 1024 )" | bc -l)" "$(echo "$mem_used \
+	* 100 / $mem_total" | bc)"
         fi
 }
 
 avg_load() {
-    AVG_LOAD=`uptime | awk -F'[a-z]:' '{ print $2 }'`
+    AVG_LOAD=$(uptime | awk -F'[a-z]:' '{ print $2 }')
     echo "L:$AVG_LOAD"
 }
 
 dte() {
-    echo $(date +"%I:%M%p %a %b %d %G")
+    date +"%I:%M%p %a %b %d %G"
 }
 
 battery() {
 
-    CAPACITY1=`acpiconf -i 0 | grep 'Remaining capacity:' | cut -f2 | sed s/%//`
-    CAPACITY2=`acpiconf -i 1 | grep 'Remaining capacity:' | cut -f2 | sed s/%//`
-    TIME1=`acpiconf -i 0 | grep 'Remaining time:' | cut -f3`
-    TIME=`acpiconf -i 1 | grep 'Remaining time:' | cut -f3`
-    STATE1=`acpiconf -i 0 | grep 'State:' | cut -f4`
-    STATE=`acpiconf -i 1 | grep 'State:' | cut -f4`
+    CAPACITY1=$(acpiconf -i 0 | grep 'Remaining capacity:' | cut -f2 | sed s/%//)
+    CAPACITY2=$(acpiconf -i 1 | grep 'Remaining capacity:' | cut -f2 | sed s/%//)
+    TIME=$(acpiconf -i 1 | grep 'Remaining time:' | cut -f3)
+    STATE=$(acpiconf -i 1 | grep 'State:' | cut -f4)
 
-    CAPACITY=$(printf "%.0f" `echo "$CAPACITY1 + $CAPACITY2 - 100" | bc`)
+    CAPACITY=$(printf "%.0f" "$(echo "$CAPACITY1 * 0.35 + $CAPACITY2 * 0.65" | bc -l )")
 
-    if [ $STATE == "discharging" ]; then
+
+    if [ "$STATE" = "discharging" ]; then
         STATE="-"
-    elif [ $STATE == "charging" ]; then
+    elif [ $STATE = "charging" ]; then
         STATE="+"
-    elif [ $STATE == "high" ]; then
+    elif [ $STATE = "high" ]; then
         STATE="="
-    elif [ $STATE == "critical" ]; then
+    elif [ $STATE = "critical" ]; then
         STATE="!!"
     else
 	STATE="~"
     fi
 
-    if [ $STATE == "-" ]; then
+    if [ $STATE = "-" ]; then
         echo "B: $CAPACITY% [$STATE] ($TIME)"
     else
         echo "B: $CAPACITY% [$STATE]"
@@ -109,7 +102,7 @@ battery() {
 
 brand() {
     BRAND=$'\uf30c'
-    echo $BRAND
+    echo "$BRAND"
 }
 
 while true; do
